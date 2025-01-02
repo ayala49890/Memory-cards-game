a002d7d885724b620517f247ea0a7351bcc44153
ayala49890 <150679367+ayala49890@users.noreply.github.com>
2025-01-02 10:50:50 +0200

    Update script.js
    delete comments

--- Memory-Game/javaScript/script.js
+++ Memory-Game/javaScript/script.js
@@ -1,8 +1,3 @@
-// let arr=["djvbj","jhvg","ayalla"];
-// let my_arr=JSON.stringifty(arr);
-// arr.forEach(function(element) {
-//     localStorage.setItem("arr",element);
-// }, this);
 
 let name1="";
 document.querySelector(`#my_form`).addEventListener("submit", function (e) {
@@ -13,39 +8,6 @@ document.querySelector(`#my_form`).addEventListener("submit", function (e) {
     document.querySelector(`.link1`).classList.remove("dontShow");
     document.querySelector(`.link1`).classList.add("show");
     const new_div=document.createElement(`div`);
-
-   
-    // let p=localStorage.getItem(arr);
-    // let arr1=JSON.parse(arr);
-    // if(!arr1)
-    // {
-    //     arr=[name1];
-    //     let my_arr=JSON.stringifty(arr);
-    //     localStorage.setItem("arr",my_arr);
-    // }
-    // else{
-    //     let i=-1;
-    //     i= arr1.findIndex(function(value){
-    //   return value==name1;
-    //    });
-    //     if(i!=-1)
-    //         {
-            //    new_div.innerHTML=name1+`!, תודה על שחזרת למשחק! למעבר למשחק יש ללחוץ על play`; 
-    //            new_div.classList.add("new_div");
-    //             document.querySelector("#body1").append(new_div); 
-    //         }
-    //         else{
-                 new_div.innerHTML=name1+`!, תודה על שנכנסת למשחק! למעבר למשחק יש ללחוץ על play`;
-                 new_div.classList.add("new_div");
-                 document.querySelector("#body1").append(new_div);
-    //              arr1.push(name1);
-    //              arr=JSON.stringifty(arr1);
-    //              localStorage.setItem("arr",arr);
-
-    //         }
-    // }
- });
-
 
 
  function add_error_msg(input,msg)
